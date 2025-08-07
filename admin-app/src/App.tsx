a2026511748bb724272a1e5441819febed0c4248
mkatbalasd <mkatbalasd@gmail.com>
2025-08-07 15:10:08 +0300

    feat: add Supabase data provider

--- admin-app/src/App.tsx
+++ admin-app/src/App.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+import { useEffect } from 'react'
+import { Admin } from 'react-admin'
+import { createTheme } from '@mui/material'
+import supabaseDataProvider from './data/supabaseDataProvider'
 
 const theme = createTheme({
   direction: 'rtl',
@@ -11,38 +10,12 @@ const theme = createTheme({
   },
 })
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   useEffect(() => {
     document.documentElement.dir = 'rtl'
   }, [])
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </ThemeProvider>
-  )
+  return <Admin dataProvider={supabaseDataProvider()} theme={theme} />
 }
 
 export default App
