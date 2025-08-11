27355c5b7ad909e9214750444fa6c33c55fd5169
mkatbalasd <mkatbalasd@gmail.com>
2025-08-11 18:25:59 +0300

    feat: add quick create buttons for license type and city

--- admin-app/src/CityQuickCreate.tsx
+++ admin-app/src/CityQuickCreate.tsx
@@ -1,19 +1,10 @@
-import { Create, SimpleForm, TextInput } from 'react-admin'
-import { useFormContext } from 'react-hook-form'
+import { SimpleForm, TextInput } from 'react-admin'
 
-const CityQuickCreate = () => {
-  const { setValue } = useFormContext()
-  const onSuccess = (data: { id: number }) => {
-    setValue('license_city_id', data.id)
-  }
-  return (
-    <Create resource="city" mutationOptions={{ onSuccess }}>
-      <SimpleForm>
-        <TextInput source="name_ar" />
-        <TextInput source="name_en" />
-      </SimpleForm>
-    </Create>
-  )
-}
+const CityQuickCreate = () => (
+  <SimpleForm>
+    <TextInput source="name_ar" />
+    <TextInput source="name_en" />
+  </SimpleForm>
+)
 
 export default CityQuickCreate
