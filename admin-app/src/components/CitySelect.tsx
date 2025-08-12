155fbf44469db894a8aa94676982e34231c8a06b
mkatbalasd <mkatbalasd@gmail.com>
2025-08-12 09:36:19 +0300

    feat: add inline add forms to selects

--- admin-app/src/components/CitySelect.tsx
+++ admin-app/src/components/CitySelect.tsx
@@ -1,7 +1,26 @@
+import { Form, Input } from 'antd'
 import AddableSelect from './AddableSelect'
 
 const CitySelect = (props: Record<string, unknown>) => (
-  <AddableSelect resource="city" label="name_ar" {...props} />
+  <AddableSelect
+    resource="city"
+    label="name_ar"
+    formFields={
+      <>
+        <Form.Item
+          name="name_ar"
+          label="الاسم بالعربية"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="name_en" label="الاسم بالإنجليزية">
+          <Input />
+        </Form.Item>
+      </>
+    }
+    {...props}
+  />
 )
 
 export default CitySelect
--- admin-app/src/components/SupplierSelect.tsx
+++ admin-app/src/components/SupplierSelect.tsx
@@ -1,7 +1,17 @@
+import { Form, Input } from 'antd'
 import AddableSelect from './AddableSelect'
 
 const SupplierSelect = (props: Record<string, unknown>) => (
-  <AddableSelect resource="supplier" label="name" {...props} />
+  <AddableSelect
+    resource="supplier"
+    label="name"
+    formFields={
+      <Form.Item name="name" label="اسم المورد" rules={[{ required: true }]}> 
+        <Input />
+      </Form.Item>
+    }
+    {...props}
+  />
 )
 
 export default SupplierSelect
