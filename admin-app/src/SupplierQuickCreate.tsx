36cf32768e41afda8ec66d4dc31fa87722f4ca9b
mkatbalasd <mkatbalasd@gmail.com>
2025-08-12 08:11:38 +0300

    feat: add quick create dialog components

--- admin-app/src/SupplierQuickCreate.tsx
+++ admin-app/src/SupplierQuickCreate.tsx
@@ -1,9 +1,31 @@
-import { SimpleForm, TextInput } from 'react-admin'
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  useCreateSuggestionContext,
+} from 'react-admin'
+import { Dialog, DialogTitle, DialogContent } from '@mui/material'
 
-const SupplierQuickCreate = () => (
-  <SimpleForm>
-    <TextInput source="name" />
-  </SimpleForm>
-)
+const SupplierQuickCreate = () => {
+  const { onClose, onSuccess, filter } = useCreateSuggestionContext()
+  return (
+    <Dialog open onClose={onClose} fullWidth>
+      <DialogTitle>إضافة جديد</DialogTitle>
+      <DialogContent>
+        <Create resource="supplier">
+          <SimpleForm
+            defaultValues={filter}
+            onSuccess={data => {
+              onSuccess(data)
+              onClose()
+            }}
+          >
+            <TextInput source="name" />
+          </SimpleForm>
+        </Create>
+      </DialogContent>
+    </Dialog>
+  )
+}
 
 export default SupplierQuickCreate
