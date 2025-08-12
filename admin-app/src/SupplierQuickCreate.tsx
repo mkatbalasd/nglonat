0d1005a510d6e06b3ecbc3946318de683b2e6a7c
mkatbalasd <mkatbalasd@gmail.com>
2025-08-12 07:46:02 +0300

    Add supplier quick create form

---
+++ admin-app/src/SupplierQuickCreate.tsx
@@ -0,0 +1,9 @@
+import { SimpleForm, TextInput } from 'react-admin'
+
+const SupplierQuickCreate = () => (
+  <SimpleForm>
+    <TextInput source="name" />
+  </SimpleForm>
+)
+
+export default SupplierQuickCreate
