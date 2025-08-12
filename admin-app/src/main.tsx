cc5386de0d6b9d756f49a52e3c6e0a5a6d45e519
mkatbalasd <mkatbalasd@gmail.com>
2025-08-12 19:16:28 +0300

    feat: restructure main with MUI theme

--- admin-app/src/main.tsx
+++ admin-app/src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import App from './App';
 
-const theme = createTheme({ direction: 'rtl' })
+const theme = createTheme({ direction: 'rtl' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
